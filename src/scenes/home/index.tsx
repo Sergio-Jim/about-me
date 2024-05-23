02b1e322f3801331d6efabb0b89543d2caeafb33
JimSergio <jimmy1sergio2@gmail.com>
2024-05-23 20:12:28 +0200

    ads

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -107,19 +107,9 @@ const Home = ({ setSelectedPage }: Props) => {
       )}
 
       {/* FOOTER FOR ADS */}
-      {areAdsVisible && (
-        <footer className="fixed bottom-0 z-30 w-full bg-primary-100 py-4 drop-shadow">
-          <div className={`${flexBetween} mx-auto w-5/6`}>
-            <div className="flex w-full justify-center">
-              <div className="flex gap-4">
-                <div className="">
-                  <AdComponent />
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
-      )}
+      <div className="">
+        <AdComponent />
+      </div>
 
       {/* Button to toggle ads */}
       <div className="fixed bottom-16 right-16 z-40">
